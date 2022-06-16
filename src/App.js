88ea6418e9b9971142cb1ddd7dc36a5671551771
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2022-06-16 23:37:12 +0200

    UPDATE: minor changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  //wrtite a function to make a get request using axios
+  const getData = () => {
+    //make a get request to the url with the request mode setted to no cors
+    axios.get('https://mangadb.pagekite.me/manga', {
+      mode: 'no-cors',
+    })
+      .then(res => {
+        console.log(res);
+      }
+      )
+      .catch(err => {
+        console.log(err);
+      }
+      );
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
